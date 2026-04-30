36782d965ba137830f084c86f83936d7e3d56ecb
George <jorgenriquemartinezc@gmail.com>
2026-04-29 22:41:33 -0500

    solución de las imagenes que se subieron

--- js/productos.js
+++ js/productos.js
@@ -2,153 +2,32 @@ const productos = [
     // Productos ADIDAS
     {
         id: 101,
-        nombre: "Adidas Adistar",
+        nombre: "Adidas Ultraboost 22",
         marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_ULTRABOOST_MS1.jpeg",
-        referencia: "MS1",
-        precio: 185000,
+        genero: "unisex",
+        imagen: "./tienda_triplea/img/hombres/Adidas/ADIDAS_ULTRABOOST_MS1.jpeg",
+        referencia: "ADB001",
+        precio: 180000,
         descripcion: "Zapatillas de alto rendimiento con tecnología boost"
     },
     {
         id: 102,
-        nombre: "Adidas Adistar",
+        nombre: "Adidas Stan Smith",
         marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_ADISTAR_MS2.jpeg",
-        referencia: "MS2",
-        precio: 185000,
-        descripcion: "Clásico diseño de zapatillas de moda"
-    },
-    {
-        id: 103,
-        nombre: "Adidas Blaze",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_BLAZE_MS1.jpeg",
-        referencia: "MS1",
-        precio: 180000,
-        descripcion: "Zapatillas de alto rendimiento con tecnología boost"
-    },
-    {
-        id: 104,
-        nombre: "Adidas Blaze",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_BLAZE_MS2.jpeg",
-        referencia: "MS2",
-        precio: 180000,
-        descripcion: "Clásico diseño de zapatillas de moda"
-    },
-    {
-        id: 105,
-        nombre: "Adidas Bounce",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_BOUNCE_MS1.jpeg",
-        referencia: "MS1",
-        precio: 185000,
-        descripcion: "Zapatillas de alto rendimiento con tecnología boost"
-    },
-    {
-        id: 106,
-        nombre: "Adidas Euipm Nt",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_EQUIPM_NT_MS1.jpeg",
-        referencia: "MS1",
-        precio: 190000,
-        descripcion: "Clásico diseño de zapatillas de moda"
-    },
-    {
-        id: 107,
-        nombre: "Adidas Equipm Nt",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_EQUIPM_NT_MS2.jpeg",
-        referencia: "MS2",
-        precio: 190000,
-        descripcion: "Zapatillas de alto rendimiento con tecnología boost"
-    },
-    {
-        id: 108,
-        nombre: "Adidas Niteball",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_NITEBALL_MS1.jpeg",
-        referencia: "MS1",
-        precio: 190000,
-        descripcion: "Clásico diseño de zapatillas de moda"
-    },
-    {
-        id: 109,
-        nombre: "Adidas Niteball",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_NITEBALL_MS2.jpeg",
-        referencia: "MS2",
-        precio: 190000,
-        descripcion: "Zapatillas de alto rendimiento con tecnología boost"
-    },
-    {
-        id: 110,
-        nombre: "Adidas Niteball",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_NITEBALL_MS3.jpeg",
-        referencia: "MS3",
-        precio: 190000,
-        descripcion: "Clásico diseño de zapatillas de moda"
-    },
-    {
-        id: 111,
-        nombre: "Adidas Samba",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_SAMBA_MS1.jpeg",
-        referencia: "MS1",
-        precio: 175000,
-        descripcion: "Zapatillas de alto rendimiento con tecnología boost"
-    },
-    {
-        id: 112,
-        nombre: "Adidas Ultraboost",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_ULTRABOOST_MS1.jpeg",
-        referencia: "MS1",
-        precio: 180000,
-        descripcion: "Clásico diseño de zapatillas de moda"
-    },
-    {
-        id: 113,
-        nombre: "Adidas Ultraboost",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_ULTRABOOST_MS2.jpeg",
-        referencia: "MS2",
-        precio: 180000,
-        descripcion: "Zapatillas de alto rendimiento con tecnología boost"
-    },
-    {
-        id: 114,
-        nombre: "Adidas Yeezy",
-        marca: "ADIDAS",
-        genero: "Caballero",
-        imagen: "/tienda_triplea/img/hombres/Adidas/ADIDAS_YEEZY_MS1.jpeg",
-        referencia: "MS1",
-        precio: 185000,
+        genero: "unisex",
+        imagen: "./tienda_triplea/img/hombres/Adidas/ADIDAS_SAMBA_MS1.jpeg",
+        referencia: "ADB002",
+        precio: 120000,
         descripcion: "Clásico diseño de zapatillas de moda"
     },
     
-
     // Productos ARMANI
     {
         id: 201,
         nombre: "Armani Exchange XA-7044",
         marca: "ARMANI",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Armani/ARMANI_EXCHANGE_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Armani/ARMANI_EXCHANGE_SHOE_MS1.jpeg",
         referencia: "ARM001",
         precio: 850000,
         descripcion: "Calzado elegante de diseñador"
@@ -160,7 +39,7 @@ const productos = [
         nombre: "Asics Gel-Kayano 28",
         marca: "ASICS",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Asics/ASICS_GEL_KAYANO_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Asics/ASICS_GEL_KAYANO_MS1.jpeg",
         referencia: "ASC001",
         precio: 450000,
         descripcion: "Zapatillas de running con máxima estabilidad"
@@ -172,7 +51,7 @@ const productos = [
         nombre: "Caterpillar Felton",
         marca: "CATERPILLAR",
         genero: "hombre",
-        imagen: "/tienda_triplea/img/hombres/Caterpillar/CATERPILLAR_BOOT_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Caterpillar/CATERPILLAR_BOOT_MS1.jpeg",
         referencia: "CAT001",
         precio: 320000,
         descripcion: "Botas resistentes de estilo industrial"
@@ -184,7 +63,7 @@ const productos = [
         nombre: "Coach Cassidy Loafer",
         marca: "COACH",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Coach/COACH_LOAFER_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Coach/COACH_LOAFER_MS1.jpeg",
         referencia: "COA001",
         precio: 750000,
         descripcion: "Mocasines elegantes de cuero genuino"
@@ -196,7 +75,7 @@ const productos = [
         nombre: "Converse Chuck Taylor All Star",
         marca: "CONVERSE",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Converse/CONVERSE_CHUCK_TAYLOR_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Converse/CONVERSE_CHUCK_TAYLOR_MS1.jpeg",
         referencia: "CON001",
         precio: 180000,
         descripcion: "Iconicas zapatillas casuales"
@@ -208,7 +87,7 @@ const productos = [
         nombre: "Diesel Izzo High",
         marca: "DIESEL",
         genero: "hombre",
-        imagen: "/tienda_triplea/img/hombres/Diesel/DIESEL_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Diesel/DIESEL_SHOE_MS1.jpeg",
         referencia: "DIE001",
         precio: 480000,
         descripcion: "Zapatillas urbanas de corte alto"
@@ -220,7 +99,7 @@ const productos = [
         nombre: "Dolce & Gabbana DG6191",
         marca: "DOLCE & GABBANA",
         genero: "hombre",
-        imagen: "/tienda_triplea/img/hombres/Dolce & Gabbana/DOLCE_GABBANA_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Dolce & Gabbana/DOLCE_GABBANA_SHOE_MS1.jpeg",
         referencia: "DOG001",
         precio: 1200000,
         descripcion: "Zapatillas de diseñador con estampado icónico"
@@ -232,7 +111,7 @@ const productos = [
         nombre: "Fila Disruptor II",
         marca: "FILA",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Fila/FILA_DISRUPTOR_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Fila/FILA_DISRUPTOR_MS1.jpeg",
         referencia: "FIL001",
         precio: 280000,
         descripcion: "Zapatillas chunky de estilo retro"
@@ -244,7 +123,7 @@ const productos = [
         nombre: "Guayo Clásico",
         marca: "GUAYO",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Guayo/GUAYO_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Guayo/GUAYO_SHOE_MS1.jpeg",
         referencia: "GUA001",
         precio: 95000,
         descripcion: "Calzado tradicional colombiano"
@@ -256,7 +135,7 @@ const productos = [
         nombre: "Hoka One One Bondi 7",
         marca: "HOKA",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Hoka/HOKA_BONDI_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Hoka/HOKA_BONDI_MS1.jpeg",
         referencia: "HOK001",
         precio: 420000,
         descripcion: "Zapatillas de running con máxima amortiguación"
@@ -268,7 +147,7 @@ const productos = [
         nombre: "Hugo Boss Axial Run",
         marca: "HUGO BOSS",
         genero: "hombre",
-        imagen: "/tienda_triplea/img/hombres/Hugo Boos/HUGO_BOSS_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Hugo Boos/HUGO_BOSS_SHOE_MS1.jpeg",
         referencia: "HUB001",
         precio: 650000,
         descripcion: "Zapatos deportivos de diseño elegante"
@@ -280,7 +159,7 @@ const productos = [
         nombre: "Lacoste Carnaby Evo",
         marca: "LACOSTE",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Lacoste/LACOSTE_SNEAKER_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Lacoste/LACOSTE_SNEAKER_MS1.jpeg",
         referencia: "LAC001",
         precio: 380000,
         descripcion: "Zapatillas casuales con el legendario cocodrilo"
@@ -292,7 +171,7 @@ const productos = [
         nombre: "Le Coq Sportif LCS R500",
         marca: "LE COQ SPORTIF",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/le coq sportif/LE_COQ_SPORTIF_SNEAKER_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/le coq sportif/LE_COQ_SPORTIF_SNEAKER_MS1.jpeg",
         referencia: "LCS001",
         precio: 320000,
         descripcion: "Zapatillas de estilo retro francés"
@@ -304,7 +183,7 @@ const productos = [
         nombre: "Louis Vuitton Archlight",
         marca: "LOUIS VUITTON",
         genero: "hombre",
-        imagen: "/tienda_triplea/img/hombres/Louis Vuitton/LOUIS_VUITTON_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Louis Vuitton/LOUIS_VUITTON_SHOE_MS1.jpeg",
         referencia: "LV001",
         precio: 1500000,
         descripcion: "Zapatillas de diseñador con silueta única"
@@ -316,7 +195,7 @@ const productos = [
         nombre: "New Balance 574 Classic",
         marca: "NEW BALANCE",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/New Balance/NEW_BALANCE_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/New Balance/NEW_BALANCE_SHOE_MS1.jpeg",
         referencia: "NB001",
         precio: 350000,
         descripcion: "Zapatillas clásicas de estilo retro"
@@ -328,7 +207,7 @@ const productos = [
         nombre: "Nike Air Max 270",
         marca: "NIKE",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Nike/NIKE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Nike/NIKE_MS1.jpeg",
         referencia: "NIK001",
         precio: 420000,
         descripcion: "Zapatillas con unidad Air Max visible"
@@ -338,7 +217,7 @@ const productos = [
         nombre: "Nike Air Force 1",
         marca: "NIKE",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Nike/NIKE_MS31.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Nike/NIKE_MS31.jpeg",
         referencia: "NIK002",
         precio: 380000,
         descripcion: "Iconicas zapatillas de baloncesto"
@@ -350,7 +229,7 @@ const productos = [
         nombre: "On Cloudmonster",
         marca: "ON CLOUD",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/On Cloud/5.jpg",
+        imagen: "./tienda_triplea/img/hombres/On Cloud/5.jpg",
         referencia: "ONC001",
         precio: 520000,
         descripcion: "Zapatillas con tecnología de amortiguación CloudTec"
@@ -362,7 +241,7 @@ const productos = [
         nombre: "Puma RS-X Reinvention",
         marca: "PUMA",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Puma/PUMA_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Puma/PUMA_SHOE_MS1.jpeg",
         referencia: "PUM001",
         precio: 350000,
         descripcion: "Zapatillas chunky con diseño audaz"
@@ -374,7 +253,7 @@ const productos = [
         nombre: "Reebok Classic Leather",
         marca: "REEBOK",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Reebook/REEBOK_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Reebook/REEBOK_SHOE_MS1.jpeg",
         referencia: "REE001",
         precio: 280000,
         descripcion: "Zapatillas clásicas de estilo retro"
@@ -386,7 +265,7 @@ const productos = [
         nombre: "Skechers Go Walk",
         marca: "SKECHERS",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Skechers/SKECHERS_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Skechers/SKECHERS_SHOE_MS1.jpeg",
         referencia: "SKE001",
         precio: 250000,
         descripcion: "Zapatillas cómodas para caminar"
@@ -398,7 +277,7 @@ const productos = [
         nombre: "Timberland 6-Inch Premium",
         marca: "TIMBERLAND",
         genero: "hombre",
-        imagen: "/tienda_triplea/img/hombres/Timberland/TIMBERLAND_BOOT_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Timberland/TIMBERLAND_BOOT_MS1.jpeg",
         referencia: "TIM001",
         precio: 550000,
         descripcion: "Botas icónicas resistentes al agua"
@@ -410,7 +289,7 @@ const productos = [
         nombre: "Tommy Hilfiger Flag Sneaker",
         marca: "TOMMY HILFIGER",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Tommy Hilfiger/TOMMY_HILFIGER_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Tommy Hilfiger/TOMMY_HILFIGER_SHOE_MS1.jpeg",
         referencia: "TOM001",
         precio: 420000,
         descripcion: "Zapatillas con logo distintivo"
@@ -422,7 +301,7 @@ const productos = [
         nombre: "Under Armour HOVR Phantom 2",
         marca: "UNDER ARMOUR",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Under Armour/UNDER_ARMOUR_SHOE_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Under Armour/UNDER_ARMOUR_SHOE_MS1.jpeg",
         referencia: "UAM001",
         precio: 480000,
         descripcion: "Zapatillas de running con tecnología de conexión"
@@ -434,7 +313,7 @@ const productos = [
         nombre: "Vans Old Skool",
         marca: "VANS",
         genero: "unisex",
-        imagen: "/tienda_triplea/img/hombres/Vans/VANS_OLD_SKOOL_MS1.jpeg",
+        imagen: "./tienda_triplea/img/hombres/Vans/VANS_OLD_SKOOL_MS1.jpeg",
         referencia: "VAN001",
         precio: 220000,
         descripcion: "Zapatillas clásicas con banda lateral distintiva"
